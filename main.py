af4c23fe668d0151da2274ae2cf719abe71e5792
Filipe Vidal <filipe.moreirav@gmail.com>
2026-06-21 00:43:55 -0300

    ADD: py_requirements.txt

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from algorithms.lfu import LFU
 from algorithms.lru import LRU
 from controllers.simulator import Simulator
 from views.console import ConsoleView
-from time import sleep
 
 def run_simulation(algorithm_class, capacity: int, references: list[int]):
     algo = algorithm_class(capacity=capacity)
@@ -22,17 +21,17 @@ def main():
     references = [1, 2, 3, 1, 4, 2, 5, 1]
 
     print("x*" * 20)
-    print("Running FIFO Simulation:")
+    print("Executando Simulação FIFO:")
     print("x*" * 20 + "\n")
     run_simulation(FCFS, capacity=3, references=references)
     
     print("\n\n" + "x*" * 20)
-    print("Running LRU Simulation:")
+    print("Executando Simulação LRU:")
     print("x*" * 20 + "\n")
     run_simulation(LRU, capacity=3, references=references)
 
     print("\n\n" + "x*" * 20)
-    print("Running LFU Simulation:")
+    print("Executando Simulação LFU:")
     print("x*" * 20 + "\n")
     run_simulation(LFU, capacity=3, references=references)
 
